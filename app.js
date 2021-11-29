62318a6b07dcb306faf6ca144092f9ffcd23e4fb
athiecheng <72422874+athiecheng@users.noreply.github.com>
2021-11-29 14:46:41 -0800

    restful api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const JobDetail = require('./models/jobDetails');
 
 mongoose.connect('mongodb://localhost:27017/job-search', {
@@ -20,18 +21,48 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
 app.get('/', (req, res) =>{
     res.render('home')
 })
 
-app.get('/job-details', async (req, res) => {
+app.get('/jobs', async (req, res) => {
     const jobs = await JobDetail.find({});
     res.render('jobs/index', {jobs})
 })
 
-app.get('/job-details/:id', async (req, res) => {
+app.get('/jobs/new', async (req, res) => {
+    res.render('jobs/new');
+});
+
+app.post('/jobs', async (req, res) => {
+    const job = new JobDetail(req.body.job);
+    await job.save();
+    res.redirect(`/jobs/${job._id}`)
+});
+
+app.get('/jobs/:id', async (req, res) => {
     const job = await JobDetail.findById(req.params.id)
     res.render('jobs/detail', {job})
+});
+
+app.get('/jobs/:id/edit', async (req, res) => {
+    const job = await JobDetail.findById(req.params.id)
+    res.render('jobs/edit', {job})
+});
+
+app.put('/jobs/:id', async (req, res) => {
+    const {id} = req.params;
+    const job = await JobDetail.findByIdAndUpdate(id, {...req.body.job});
+    res.redirect(`/jobs/${job._id}`)
+})
+
+app.delete('/jobs/:id', async (req, res) => {
+    const {id} = req.params;
+    await JobDetail.findByIdAndDelete(id);
+    res.redirect('/jobs')
 })
 
 app.listen(3000, ()=> {
