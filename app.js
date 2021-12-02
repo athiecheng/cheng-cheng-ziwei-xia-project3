7a2e3045d0176fcd677deadf242bc91be727da85
athiecheng <72422874+athiecheng@users.noreply.github.com>
2021-12-02 13:03:41 -0800

    organize routes

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsmate = require('ejs-mate');
-const {jobSchema} = require('./joiSchemas');
-const catchAsync = require('./helpers/catchAsyncError');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./helpers/ExpressError');
 const methodOverride = require('method-override');
-const JobDetail = require('./models/jobDetails');
-
+const jobsRoutes = require('./routes/JobsRouters')
 mongoose.connect('mongodb://localhost:27017/job-search', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
 
 const db = mongoose.connection;
@@ -28,59 +27,31 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-//middleware to validate job
-const validateJob = (req, res, next) => {
-    const {error} = jobSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
+const sessionConfig = {
+    secret:'somesecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use((req, res, next)=> {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/jobs', jobsRoutes)
 
 app.get('/', (req, res) =>{
     res.render('home')
 })
-
-app.get('/jobs', async (req, res) => {
-    const jobs = await JobDetail.find({});
-    res.render('jobs/index', {jobs})
-})
-
-app.get('/jobs/new', async (req, res) => {
-    res.render('jobs/new');
-});
-
-app.post('/jobs', validateJob,catchAsync(async (req, res, next) => {
-    // if (!req.body.job) throw new ExpressError('Invalid job data', 400);
-    const job = new JobDetail(req.body.job);
-    await job.save();
-    res.redirect(`/jobs/${job._id}`)
-}));
-
-app.get('/jobs/:id', catchAsync(async (req, res) => {
-    const job = await JobDetail.findById(req.params.id)
-    res.render('jobs/detail', {job})
-}));
-
-app.get('/jobs/:id/edit', catchAsync(async (req, res) => {
-    const job = await JobDetail.findById(req.params.id)
-    res.render('jobs/edit', {job})
-}));
-
-app.put('/jobs/:id', validateJob, catchAsync(async (req, res) => {
-    const {id} = req.params;
-    const job = await JobDetail.findByIdAndUpdate(id, {...req.body.job});
-    res.redirect(`/jobs/${job._id}`)
-}));
-
-app.delete('/jobs/:id', catchAsync(async (req, res) => {
-    const {id} = req.params;
-    await JobDetail.findByIdAndDelete(id);
-    res.redirect('/jobs')
-}));
 
 app.all('*', (req, res, next)=> {
     next(new ExpressError('Page Not Found', 404))
